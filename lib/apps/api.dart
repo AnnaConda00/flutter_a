40be4b427331038ba517ae7e2867670e41866638
AnnaConda00 <a70886197@gmail.com>
2025-01-30 07:14:09 +0300

    Update api.dart

--- lib/apps/api.dart
+++ lib/apps/api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../model/product.dart';
 import '../model/category.dart';
-
+ 
 class ApiService {
   final String productApiUrl =
       'http://onlinestore.whitetigersoft.ru/api/common/product/details?appKey=EyZ6DhtHN24DjRJofNZ7BijpNsAZ-TT1is4WbJb9DB7m83rNQCZ7US0LyUg5FCP4eoyUZXmM1z45hY5fIC-JTCgmqHgnfcevkQQpmxi8biwwlSn0zZedvlNh0QkP1-Um';
